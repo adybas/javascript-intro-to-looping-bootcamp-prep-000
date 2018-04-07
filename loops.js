4b5bbaf2380c964439226653e74f6a154f4f9083
Anna Dybas <a.m.dybas@gmail.com>
2018-04-07 18:33:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
 function forLoop(array) {
-  if (
-    for (; i === 1;) {
-    return "I am 1 strange loops."; 
+  for (let i = 0; i < 25; i++)
+  if (i === 1;) {
+    return "I am 1 strange loop."; 
     }
   ) else if (
-    for (let i = 0; i < 25; i++) {
+   {
     return "I am ${i} strange loops.";
     }
    )
+  }
   }
