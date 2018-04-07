5bde1159c6cf581beed7cc8a8c830b3bd1429381
Anna Dybas <a.m.dybas@gmail.com>
2018-04-07 20:12:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,6 +24,6 @@ function doWhileLoop(array) {
 }
   do {
     console.log('doo-bee-doo-bee-doo')
-  } while (array.length - 1 > 0 && maybeTrue());
+  } while (array.length > 0 && maybeTrue());
   return array;
 }
