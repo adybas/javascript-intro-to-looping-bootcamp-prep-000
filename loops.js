886edfe055d2c11236d7e25ed583f91be5339bac
Anna Dybas <a.m.dybas@gmail.com>
2018-04-07 19:55:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,9 +11,9 @@ function forLoop(array) {
   return array;
 }
  
-function whileLoop(number) {
-  while (number > 0) {
-    console.log(--number)
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
   }
-  
+  return n.push('done');
 }
